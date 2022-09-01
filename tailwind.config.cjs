3930a225dab4f0f840c727e27d0b87c583a466a0
lollipop-onl <lollipop@simochee.net>
2022-09-01 15:17:18 +0900

    ✨ setup astro

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = config;
